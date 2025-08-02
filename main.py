3293b80467b134648a4fa9c6a542774c93c8ce47
JairVG <velasco.jair92@gmail.com>
2025-08-01 19:01:39 -0600

    feat(api): create messages endpoints

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from routes import contacts, groups
+from routes import contacts, groups, messages
 
 app = FastAPI()
 
 # Incluir routers
 app.include_router(contacts.router)
 app.include_router(groups.router)
+app.include_router(messages.router)
