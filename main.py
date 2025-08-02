b8981051c3eb180dd4aa7b49e3f861ae44ff63fc
JairVG <velasco.jair92@gmail.com>
2025-08-01 23:24:06 -0600

    feat(backend): finish the backend

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routes import contacts, groups, messages
-
-app = FastAPI()
-
-# Incluir routers
-app.include_router(contacts.router)
-app.include_router(groups.router)
-app.include_router(messages.router)
